927f97ca666bd63729fc0da8910abfeaecd830fc
Dima <dmytro.vasylkov@appsflyer.com>
2019-02-27 17:11:46 +0200

    Mini

--- src/main/kotlin/Apps.kt
+++ src/main/kotlin/Apps.kt
@@ -1,6 +1,6 @@
 fun search(data: Map<String, Any>, key: String): Any? =
-    data[key] ?: data.values
-        .filter { it is Map<*, *> }
-        .map { @Suppress("UNCHECKED_CAST") (it as Map<String, Any>) }
-        .mapNotNull { search(it, key) }
-        .first()
+        data[key] ?: data.values
+                .filter { it is Map<*, *> }
+                .map { @Suppress("UNCHECKED_CAST") (it as Map<String, Any>) }
+                .mapNotNull { search(it, key) }
+                .first()
--- src/test/kotlin/SearchTest.kt
+++ src/test/kotlin/SearchTest.kt
@@ -2,15 +2,11 @@ import com.fasterxml.jackson.databind.ObjectMapper
 import org.apache.commons.io.IOUtils
 import org.junit.Assert
 import org.junit.Test
-import org.junit.runner.RunWith
-import org.junit.runners.JUnit4
 import java.nio.charset.Charset
 
-@RunWith(JUnit4::class)
 class SearchTest {
     @Test
-    fun test() =
-        IOUtils.toString(javaClass.getResource("data.json"), Charset.defaultCharset())
+    fun test() = IOUtils.toString(javaClass.getResource("data.json"), Charset.defaultCharset())
             .run { ObjectMapper().readValue(this, Map::class.java) }
             .run { @Suppress("UNCHECKED_CAST") search(this as Map<String, Any>, "af") }
             .run { Assert.assertEquals("ahah", this) }
