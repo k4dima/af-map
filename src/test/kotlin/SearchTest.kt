72855ce5a7b6c8ab94eb3ee4963d16775039c9f9
Dmytro Vasylkov <dmytro.vasylkov@appsflyer.com>
2019-08-19 16:14:02 +0300

    Gson type inference

--- src/test/kotlin/SearchTest.kt
+++ src/test/kotlin/SearchTest.kt
@@ -1,5 +1,4 @@
 import com.google.gson.Gson
-import com.google.gson.reflect.TypeToken
 import org.apache.commons.io.IOUtils
 import org.junit.Assert
 import org.junit.Test
@@ -8,7 +7,7 @@ import java.nio.charset.Charset
 class SearchTest {
     @Test
     fun test() = IOUtils.toString(javaClass.getResource("data.json"), Charset.defaultCharset())
-            .let { Gson().fromJson<Map<String, Any>>(it, object : TypeToken<Map<String, Any>>() {}.type) }
+            .let { Gson().fromJson<Map<String, Any>>(it, Map::class.java) }
             .let { search(it, "af") }
             .let { Assert.assertEquals("ahah", it) }
 }
